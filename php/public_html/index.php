18ffe214e935c6a98561abc57c00cb3bba6a9e59
prahaladh <prahaladhhn96@gmail.com>
2019-05-20 10:18:18 +0530

    Update index.php

--- php/public_html/index.php
+++ php/public_html/index.php
@@ -1,61 +1,24 @@
 <?php
-   $dbhost = 'mysql';
-   $dbuser = 'root';
-   $dbpass = 'root';
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo 'Connected successfully';
-   
-   $sql = 'CREATE Database test_db';
-   $retval = mysql_query( $sql, $conn );
-   
-   if(! $retval ) {
-      die('Could not create database: ' . mysql_error());
-   }
-   
-   echo "Database test_db created successfully\n";
-   mysql_close($conn);
-   
-   
-   $conn = mysql_connect($dbhost, $dbuser, $dbpass);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo 'Connected successfully';
-   
-       $sql = 'CREATE TABLE employee( '.
+$con=mysqli_connect("localhost","my_user","my_password","my_db");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+  mysqli_query($con,"CREATE Database test_db");
+  
+  mysqli_query($con,"CREATE TABLE employee( '.
       'emp_id INT NOT NULL AUTO_INCREMENT, '.
       'emp_name VARCHAR(20) NOT NULL, '.
       'emp_address  VARCHAR(20) NOT NULL, '.
       'emp_salary   INT NOT NULL, '.
       'join_date    timestamp(14) NOT NULL, '.
-      'primary key ( emp_id ))';
-   mysql_select_db('test_db');
-   $retval = mysql_query( $sql, $conn );
-   
-   if(! $retval ) {
-      die('Could not create database: ' . mysql_error());
-   }
-   
-   echo "Database test_db created successfully\n";
-   mysql_close($conn);
-   
-   if(! $conn ) {
-      die('Could not connect: ' . mysql_error());
-   }
-   
-   echo 'Connected successfully';
-   
-$sql = "SELECT emp_id, emp_name, emp_salary FROM employee";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
+      'primary key ( emp_id ))");
+	  
+ $result = mysqli_query($con,"SELECT emp_id, emp_name, emp_salary FROM employee");
+  
+ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "id: " . $row["emp_id"]. " - Name: " . $row["emp_name"]. " " . $row["emp_salary"]. "<br>";
@@ -63,12 +26,6 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-   
-   if(! $retval ) {
-      die('Could not create database: ' . mysql_error());
-   }
-   
-   echo "Database test_db created successfully\n";
-   mysql_close($conn);
-   
+  
+mysqli_close($con);
 ?>
