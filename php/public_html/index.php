ca3cff2438bdfa04aafe7d4097b94a1c952ce052
prahaladh <prahaladhhn96@gmail.com>
2019-05-22 15:07:28 +0530

    Update index.php

--- php/public_html/index.php
+++ php/public_html/index.php
@@ -47,7 +47,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["NAME"]. " AGE:" . $row["AGE"]. "<br>";
+        echo "id: " . $row["ID"]. " - Name: " . $row["NAME"]. " AGE:" . $row["AGE"]. "<br>";
     }
 } else {
     echo "0 results";
