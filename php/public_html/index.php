d5d97db22ae95ac99e1b63b22468c08bdb773005
prahaladh <prahaladhhn96@gmail.com>
2019-05-22 15:00:58 +0530

    Update index.php

--- php/public_html/index.php
+++ php/public_html/index.php
@@ -5,17 +5,15 @@ $host = 'mysql';
 $user = 'root';
 $pass = 'root';
 $conn = new mysqli($host, $user, $pass);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected to MySQL successfully!";
 
-$query="CREATE DATABASE IF NOT EXISTS my_db";
-if (mysqli_query("$query")) {
-print ("Database created successfully <br>");
-} else {
-print ("Error in creating  database: <br><br>". mysqli_error ());
+// sql to create DB
+$sql = "CREATE DATABASE IF NOT EXISTS my_db";
+if(!$conn->query($sql)){
+    echo "DB creation failed: (" . $conn->errno . ") " . $conn->error;
 }
 $dbname='my_db';
 // Create connection
@@ -24,7 +22,6 @@ $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 // sql to create table
 $sql = "CREATE TABLE IF NOT EXISTS `USERS` (
     `ID` int(11) unsigned NOT NULL auto_increment,
@@ -40,14 +37,13 @@ $sql = "INSERT INTO USERS (NAME, AGE) VALUES
             ('Clark',  '22'),
             ('John', '23'),
             ('Harry', , '24')";
-if(mysqli_query($link, $sql)){
+if(mysqli_query($conn, $sql)){
     echo "Records added successfully.";
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
 $sql = "SELECT * FROM USERS";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
